a229fad11453e374814be7d30f4cbcbc51b68496
Wxy2003-xy <auferstanden1@gmail.com>
2025-08-29 17:14:04 +0800

    wa1

--- DLC/mergeinterval.cpp
+++ DLC/mergeinterval.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    vector<vector<int>> merge_recur(vector<vector<int>>& intervals) {
         
+    }
+    vector<vector<int>> merge_br(vector<vector<int>>& intervals) {
+        if (intervals.size() < 2) {
+            return intervals;
+        }
+        vector<vector<int>> res;
+        res.push_back(intervals[0]);
+        for (int i = 0; i < intervals.size(); i++) {
+        }
+
     }
 };
